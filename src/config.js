cefc294ad9c71f3ab23c60e338019cc8fc765b2d
Yefribrioso3 <yefribrioso3@gmail.com>
2021-12-15 01:41:32 -0400

    Se actualizo el back

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export default {
     port: process.env.PORT || 3001,
     dbUSer: process.env.DB_USER || '',
     dbPassword: process.env.DB_PASSWORD || '',
-    dbServer: process.env.DB_SERVER || '',
+    dbServer: process.env.DB_SERVER,
     dbDatabase: process.env.DB_DATABASE || ''
 };
 
